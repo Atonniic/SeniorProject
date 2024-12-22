2c6aa60c8d0b7af63f75c8f45b03360fad0763f0
mkksdx2 <chaipot.k@ku.th>
2024-12-22 22:15:12 +0700

    create cleanCeas-config.sh

---
+++ ceasDataset/cleanCeas-config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name=train_nlp_model
+#SBATCH --output=train_output_%j.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --time=04:00:00
+#SBATCH --partition=gpu
+
+python cleanCeas.py
